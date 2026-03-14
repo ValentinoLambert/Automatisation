4bd6fccda054dc5a98e35888b4e042b2ff2db198
Paul5400 <paul.andrieu54@gmail.com>
2026-03-14 09:39:50 +0100

    Implémentation pattern decorator avec tests

--- cours6-design_pattern/cours6-design_pattern/design-pattern-main/decorator/Tests/ComputerDecoratorTest.php
+++ cours6-design_pattern/cours6-design_pattern/design-pattern-main/decorator/Tests/ComputerDecoratorTest.php
@@ -5,26 +5,40 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 
 use App\Laptop;
+use App\GPUDecorator;
+use App\OLEDScreenDecorator;
 
 class ComputerDecoratorTest extends TestCase
 {
     public function testBasicLaptop()
     {
         $laptop = new Laptop();
-        
+
         $this->assertSame(400, $laptop->getPrice());
         $this->assertSame("A laptop computer", $laptop->getDescription());
     }
 
     public function testLaptopWithGPU()
     {
-        // TODO: faire le test
-        $this->assertSame(true, 1 === 1);
+        $laptop = new GPUDecorator(new Laptop());
+
+        $this->assertSame(700, $laptop->getPrice());
+        $this->assertSame("A laptop computer, with GPU", $laptop->getDescription());
     }
 
     public function testLaptopWithOLEDScreen()
     {
-        // TODO: faire le test
-        $this->assertSame(false, 1 === 2);
+        $laptop = new OLEDScreenDecorator(new Laptop());
+
+        $this->assertSame(600, $laptop->getPrice());
+        $this->assertSame("A laptop computer, with OLED screen", $laptop->getDescription());
+    }
+
+    public function testLaptopWithGPUAndOLEDScreen()
+    {
+        $laptop = new OLEDScreenDecorator(new GPUDecorator(new Laptop()));
+
+        $this->assertSame(900, $laptop->getPrice());
+        $this->assertSame("A laptop computer, with GPU, with OLED screen", $laptop->getDescription());
     }
 }
