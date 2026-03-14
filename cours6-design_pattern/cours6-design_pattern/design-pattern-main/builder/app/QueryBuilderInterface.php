ce251074d019d7c41952f73ee1b3d940a8f43fc0
e7160u <valentino.lambert94@gmail.com>
2026-03-14 09:42:10 +0100

    cours6: implement builder pattern exercise

--- cours6-design_pattern/cours6-design_pattern/design-pattern-main/builder/app/QueryBuilderInterface.php
+++ cours6-design_pattern/cours6-design_pattern/design-pattern-main/builder/app/QueryBuilderInterface.php
@@ -1,5 +1,20 @@
 <?php
 
-# TODO: Créer une classe QueryBuilder en utilisant le design pattern Builder
-
 namespace App;
+
+interface QueryBuilderInterface
+{
+	public function select(array $fields): self;
+
+	public function from(string $table): self;
+
+	public function where(string $condition): self;
+
+	public function orderBy(string $field, string $direction = 'ASC'): self;
+
+	public function limit(int $limit): self;
+
+	public function getQuery(): string;
+
+	public function reset(): self;
+}
--- cours6-design_pattern/cours6-design_pattern/design-pattern-main/builder/public/index.php
+++ cours6-design_pattern/cours6-design_pattern/design-pattern-main/builder/public/index.php
@@ -1,8 +1,27 @@
 <?php
 require('../vendor/autoload.php');
 
-die('ici, vous pouvez tester votre builder !');
+use App\MySQLQueryBuilder;
 
-# TODO: Creer un QueryBuilder
-# Ecrire une requête en chainant des methodes
-# Afficher la requête
+$builder = new MySQLQueryBuilder();
+
+$query1 = $builder
+	->select(['id', 'name', 'email'])
+	->from('users')
+	->where('active = 1')
+	->orderBy('name', 'ASC')
+	->limit(10)
+	->getQuery();
+
+$query2 = $builder
+	->reset()
+	->select(['id', 'title'])
+	->from('posts')
+	->where('published = 1')
+	->where('category = "tech"')
+	->getQuery();
+
+echo '<pre>';
+echo $query1 . PHP_EOL;
+echo $query2 . PHP_EOL;
+echo '</pre>';
