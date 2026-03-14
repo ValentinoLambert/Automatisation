cf400cd57ccf92502055fb080c27fcaef55d9ad7
Paul5400 <paul.andrieu54@gmail.com>
2026-03-14 10:53:48 +0100

    Pattern observer et tests

--- cours6-design_pattern/cours6-design_pattern/design-pattern-main/observer/app/User.php
+++ cours6-design_pattern/cours6-design_pattern/design-pattern-main/observer/app/User.php
@@ -2,15 +2,17 @@
 
 namespace App;
 
-class User 
+class User implements \SplObserver
 {
-    // Hors exercice mais notable:
-    // Promotion du constructeur: https://www.php.net/manual/fr/language.oop5.decon.php#language.oop5.decon.constructor.promotion
     public function __construct(
         private string $name,
         private bool $notified = false
     ) {}
 
+    public function update(\SplSubject $subject): void
+    {
+        $this->notified = true;
+    }
 
     public function isNotified(): bool
     {
