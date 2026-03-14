6b635d9aadfdaae91075a16eb999b0e794ff9802
e7160u <valentino.lambert94@gmail.com>
2026-03-14 10:56:12 +0100

    factory

--- cours6-design_pattern/cours6-design_pattern/design-pattern-main/factory/public/index.php
+++ cours6-design_pattern/cours6-design_pattern/design-pattern-main/factory/public/index.php
@@ -1,4 +1,16 @@
 <?php
 require('../vendor/autoload.php');
-die('ici, vous pouvez tester votre factory !');
-# Essayez d'utiliser votre factory ici
+
+use App\Factory\VehicleFactory;
+
+$factory = new VehicleFactory();
+
+$bicycle = $factory->create('bicycle');
+$car = $factory->create('car');
+$truck = $factory->create('truck');
+
+echo '<pre>';
+echo 'Bicycle: ' . $bicycle->getFuelType() . ' - ' . $bicycle->getCostPerKm() . ' €/km' . PHP_EOL;
+echo 'Car: ' . $car->getFuelType() . ' - ' . $car->getCostPerKm() . ' €/km' . PHP_EOL;
+echo 'Truck: ' . $truck->getFuelType() . ' - ' . $truck->getCostPerKm() . ' €/km' . PHP_EOL;
+echo '</pre>';
